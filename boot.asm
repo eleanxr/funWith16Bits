e6b11dd9f9be4a359e9dbe248cb63eac7b09c7e1
Will Dicharry <wdicharry@gmail.com>
2017-06-04 11:45:51 -0600

    Add basic video mode setup.

--- boot.asm
+++ boot.asm
@@ -13,6 +13,17 @@ start:
   mov ax, 07c0h
   mov ds, ax
 
+  ;; Set up the video mode for color output.
+  ;; 80x30 8x16 640x480 16 colors
+  mov ah, 00h ; Interrupt function code.
+  mov al, 12h ; Mode argument.
+  int 10h
+  ;; Set up a background color.
+  mov ah, 0Bh ;; Interrupt function code.
+  mov bh, 00h ;; Interrupt function code.
+  mov bl, 01h ;; Function argument goes in bh.
+  int 10h
+
   mov si, text_string
   call print_string
 
@@ -22,6 +33,7 @@ start:
 
 print_string:
   mov ah, 0Eh
+  mov bl, 0Eh
   .repeat:
     lodsb
     cmp al, 0
